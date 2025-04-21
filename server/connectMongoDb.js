38cd035230ea7f9c33cb06676c5b6774ae4a1dca
Diya Sikka <diyasikka22@gmail.com>
2025-04-21 15:12:47 +0530

    Updated MongoURL

--- server/connectMongoDb.js
+++ server/connectMongoDb.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/snipster");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
